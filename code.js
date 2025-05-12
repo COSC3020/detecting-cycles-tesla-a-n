6de2c8ec87b4c668e377c80b8941f33ee71e038a
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-12 08:02:06 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,13 @@
 function hasCycle(graph) {
+    // track visited vertices
+    // track vertices in the stack
+    // Initialize vertices as not visited
+    // Check each vertex
+        //if vertex not visited, check if it's part of a cycle
+    // Mark current vertex as visited and add to stack
+    // Check all neighbors
+        // If the neighbor not visited, recursively check it
+        // If the neighbor already in stack, cycle found
+    // Remove vertex from stack when done exploring
     return false;
 }
